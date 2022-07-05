781c332ee56abeffaf0e41b1b83f3d7d02f5e1fc
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-05 16:23:13 -0700

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,8 @@ void print_alphabet_x10(void)
 {
 	int counter = 0;
 	int letter;
-	while (counter++ <= 9)
 
+	while (counter++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
