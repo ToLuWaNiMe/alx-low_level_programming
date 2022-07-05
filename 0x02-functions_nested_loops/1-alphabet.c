2ff4ba5bcb5ec97fffd2a51ca419c05e6ce8abf7
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-05 14:59:50 -0700

    alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,12 @@
  * *print_alphabet - function that print alphabet
  * *it prints in lowercase
  * *Return: no return
-*/
+ */
 void print_alphabet(void)
 {
 	int letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
+
 	{
 		_putchar (letter);
 	}
