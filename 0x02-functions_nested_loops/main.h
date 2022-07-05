8d5215b10df44cfbf8b9ac3d845c0a7fc1b31b96
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-05 14:52:09 -0700

    alphabet function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,13 @@
-
-/**
- * *File:main.h
- * *Author:Pericles Adjovi
- * *Desc:header file containing functions declaration
- * */
-
+int _putchar(char c);
 void print_alphabet(void);
-void jack_bauer(void);
 void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
-void print_times_table(int);
-int _putchar(char);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int add(int n, int m);
-int print_last_digit(int n);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
