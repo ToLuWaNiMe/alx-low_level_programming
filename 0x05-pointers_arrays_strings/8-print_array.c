265931804ee0e2b0840657cb1fdb1648e1e9c3bf
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-12 16:36:06 -0700

    print arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,11 +3,11 @@
 
 /**
  *  * print_array - print `n` elements of an array of integers
- *   * @a: int type array pointer
- *    * @n: int type integer
- *     * Description: Numbers must be separated by comma and space.
- *      * Numbers should be displayed in the same order they are stored in array.
- *       * You can only use _putchar to print.
+ *  * @a: int type array pointer
+ *  * @n: int type integer
+ *  * Description: Numbers must be separated by comma and space.
+ * * Numbers should be displayed in the same order they are stored in array.
+ * * You can only use _putchar to print.
 **/
 
 void print_array(int *a, int n)
