4794dcd10ac749920ec4df58046236787eea387d
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-15 13:54:18 -0700

    magic file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * * print_number - prints an integer
- * *@n:integer to be printed
- * *
+ * * print_number - Prints any integer with putchar
+ * * @n: Number to prints
+ * * Return: Nothing
 **/
+
 void print_number(int n)
 {
-	unsigned int num = n;
-
+	unsigned int x;
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n *= -1;
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
