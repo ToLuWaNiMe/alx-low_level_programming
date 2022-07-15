d8f6bf5286290edd2f48ed1ebed7e36ef70554bb
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-15 10:55:20 -0700

    encodes with rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * * rot13 -  encodes a string using rot13
- * * @str:the string targeted
- * *Return: returns the encoded string
+ *  * rot13 - Encodes a string using rot13.
+ *   * @str: The string to be encoded.
+ *    *
+ *     * Return: A pointer to the encoded string.
 **/
-
 char *rot13(char *str)
 {
-	int index1, index2;
-
+	int indx1 = 0, indx2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 		'G', 'H', 'I', 'J', 'K', 'L',
 		'M', 'N', 'O', 'P', 'Q', 'R',
@@ -19,7 +18,6 @@ char *rot13(char *str)
 		'k', 'l', 'm', 'n', 'o', 'p',
 		'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z'};
-
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 		'T', 'U', 'V', 'W', 'X', 'Y',
 		'Z', 'A', 'B', 'C', 'D', 'E',
@@ -29,16 +27,20 @@ char *rot13(char *str)
 		'x', 'y', 'z', 'a', 'b', 'c',
 		'd', 'e', 'f', 'g', 'h', 'i',
 		'j', 'k', 'l', 'm'};
-	while (str[++index1])
+
+	while (str[indx1])
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (str[indx1] == alphabet[indx2])
 			{
-				str[index1] = rot13key[index2];
+				str[indx1] = rot13key[indx2];
 				break;
 			}
 		}
+
+		indx1++;
+
 	}
 	return (str);
 }
