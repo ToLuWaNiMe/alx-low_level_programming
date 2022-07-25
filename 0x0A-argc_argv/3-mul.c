a521e2cbeb482d0a23d9b36c8d7dd5c59bf131fb
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-07-25 09:44:15 -0700

    Print multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
- *  * main - Program that takes first two integer arguments and prints the product
- *   * @argc: Number of command line arguments
- *    * @argv: Array name
- *     * Return: 1 if not enough arguments passed in, 0 otherwise
+ * * main - Program that takes first 2integer arguments and prints the product
+ * * @argc: Number of command line arguments
+ * * @argv: Array name
+ * * Return: 1 if not enough arguments passed in, 0 otherwise
  **/
 
 int main(int argc, char *argv[])
