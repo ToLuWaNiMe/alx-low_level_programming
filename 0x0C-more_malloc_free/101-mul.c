9cde792f649b86ad23863a5c37f3f58afe35283c
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-08-01 10:42:30 -0700

    mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,9 +8,9 @@ void add_nums(char *final_prod, char *next_prod, int next_len);
 
 /**
  *   * find_len - Finds the length of a string.
- *     * @str: The string to be measured.
- *       *
- *         * Return: The length of the string.
+ *   * @str: The string to be measured.
+ *   *
+ *   * Return: The length of the string.
  **/
 int find_len(char *str)
 {
@@ -48,11 +48,11 @@ char *create_xarray(int size)
 }
 
 /**
- *   * iterate_zeroes - Iterates through a string of numbers containing
- *     *                             leading zeroes until it hits a non-zero number.
- *     * @str: The string of numbers to be iterate through.
- *     *
- *     * Return: A pointer to the next non-zero element.
+ *  * iterate_zeroes - Iterates through a string of numbers containing
+ *  *  leading zeroes until it hits a non-zero number.
+ *  * @str: The string of numbers to be iterate through.
+ *  *
+ * * Return: A pointer to the next non-zero element.
  **/
 char *iterate_zeroes(char *str)
 { 	
@@ -63,19 +63,20 @@ char *iterate_zeroes(char *str)
 }
 
 /**
- *   * get_digit - Converts a digit character to a corresponding int.
- *   * @c: The character to be converted.
- *   *
- *   * Description: If c is a non-digit, the function
- *   *        exits with a status of 98.
- *   * Return: The converted int.
+ ** get_digit - Converts a digit character to a corresponding int.
+ ** @c: The character to be converted.
+ **
+ ** Description: If c is a non-digit, the function
+ ** exits with a status of 98.
+ ** Return: The converted int.
  **/
 int get_digit(char c)
 {
 	int digit = c - '0';
 
 	if (digit < 0 || digit > 9)
-	{ 		printf("Error\n");
+	{
+		printf("Error\n");
 		exit(98);
 	}
 
@@ -83,14 +84,14 @@ int get_digit(char c)
 }
 
 /**
- *   * get_prod - Multiplies a string of numbers by a single digit.
- *     * @prod: The buffer to store the result.
- *   * @mult: The string of numbers.
- *   * @digit: The single digit.
- *   * @zeroes: The necessary number of leading zeroes.
- *   *
- *   * Description: If mult contains a non-digit, the function
- *   *     exits with a status value of 98.
+ * * get_prod - Multiplies a string of numbers by a single digit.
+ * * @prod: The buffer to store the result.
+ * * @mult: The string of numbers.
+ * * @digit: The single digit.
+ * * @zeroes: The necessary number of leading zeroes.
+ * *
+ * * Description: If mult contains a non-digit, the function
+ * * exits with a status value of 98.
  **/
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
@@ -177,7 +178,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  *   * @argc: An array of pointers to the arguments.
  *   *
  *  * Description: If the number of arguments is incorrect or one number
- *    *                       contains non-digits, the function exits with a status of 98.
+ ** contains non-digits, the function exits with a status of 98.
  *    * Return: Always 0.
  **/
 int main(int argc, char *argv[])
