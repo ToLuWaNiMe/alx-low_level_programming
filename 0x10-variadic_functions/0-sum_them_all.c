f9cbb79d7e1e35045bd48a1c6910d62c02f3de8f
Bahdtoluwani <oladosu.toluene@gmail.com>
2022-08-04 19:29:06 -0700

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -21,10 +21,10 @@ int sum_them_all(const unsigned int n, ...)
 	if (n != 0)
 	{
 		for (i = 0; i < n; i++)
-			sum += va_arg(li, int);
+	{
+		sum += va_arg(li, int);
 	}
 
 	va_end(li);
 	return (sum);
-
-}
+	}
